a4b6164bd2f306add78fba3c4b43a4e9de0422c7
Nicolas Chabalier <nichabalier@gmail.com>
2020-12-17 14:46:37 +0100

    Fix scripts.

--- src/main/scripts/Cast3m.bat
+++ src/main/scripts/Cast3m.bat
@@ -1,3 +1,37 @@
 @echo off
 
-castem20 %1%
+set PATH=%PATH%;C:\Cast3M\PCW_20\bin\
+
+:: echo stop to fill console when castem ask for keyboard input
+CALL echo "stop" | castem20.bat %*
+
+::<nul timeout /t 1 
+::for /F "TOKENS=1,2,*" %%a in ('tasklist /FI "IMAGENAME eq bin_Cast3M*"') do set PID_R=%%a
+::echo %PID_R% > PID
+
+:::loop
+::tasklist | find " %PID_R% " > pid.txt
+::if not errorlevel 1 (
+::    timeout /t 1 >nul
+::    goto :loop
+::)
+
+<nul timeout /t 10 
+
+for /F "TOKENS=*" %%a in ('findstr "ERREUR" out.txt') do set exit_line=%%a
+
+::echo %exit_line% > log2.txt
+
+for /f "tokens=2 delims=:" %%a in ("%exit_line%") do (
+  set exit_line_end=%%a
+)
+
+set exit_line_end=%exit_line_end: =%
+::echo %exit_line_end% >> log2.txt
+
+set exit_code=12
+for /f "tokens=1,* delims=*" %%a in ("%exit_line_end%") do (set exit_code=%%a%%b)
+
+::echo %exit_code% >> log2.txt
+
+exit %exit_code%
--- src/main/scripts/Cast3m.sh
+++ src/main/scripts/Cast3m.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-castem20 $*
+#FICH=$(basename $1)
+listing="out.txt"
+
+nice -5 castem20 $* > $listing &
+
+PID_CAST3M=$!
+echo $PID_CAST3M >> PID
+wait $PID_CAST3M
+rm -f PID
+# Get the exit code (4 lines before end of file)
+EXIT_CODE=`tail -n 4 $listing | head -n 1 | sed 's/\*//g' | cut -f2 -d ":" | sed 's/ //g'`
+echo "Exit code: $EXIT_CODE"
+exit $EXIT_CODE
